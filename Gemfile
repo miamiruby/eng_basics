bdbbc16fb813180984e9fdba2517e779b8e28875
Paul Kruger <paul@miamiphp.org>
2017-06-09 17:12:41 -0400

    Adding files

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby '2.3.1'
+
+
+gem 'awesome_print'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'byebug'
+end
--- fibs.rb
+++ fibs.rb
@@ -1,23 +1,49 @@
-# generates, stores and calculates retained fibonacci sequence numbers
-
 class Fibs
   attr_accessor :generated_fibs
 
-  def generate x
+  def generate num_of_fibs
+    generate_fibs num_of_fibs
   end
 
-  def is_known_fib? num
+  def is_known_fib? fib
+    generated_fibs.include? fib
   end
 
-  def all_fibs? seq_array
+  def all_fibs? fibs
+    fibs.each {|fib|
+      unless is_known_fib? fib
+        return false
+      end
+    }
+    true
   end
 
   def known_fibs_sum_of_squares
+    !fib_size.zero? ? fib_squares : 0
+  end
+
+  def generated_fibs
+    @generated_fibs ||= []
   end
 
   private
-
-  def generate_fibs x
-    # this method should do the work of generating the sequence
+  def fib_squares
+    fib_sq = generated_fibs.map{|fib| fib**2}
+    squares = fib_sq.inject{ |sum,fib| sum + fib }
+    squares
   end
+
+  def fib_size
+    generated_fibs.length
+  end
+
+  def generate_fibs fib_total
+    fibset = (fib_size...fib_total)
+    fibset.each { |fib|
+      fib.zero? || fib == 1 ? @generated_fibs.push(fib) : @generated_fibs.push(@generated_fibs[-1] + @generated_fibs[-2])
+
+    }
+    @generated_fibs
+  end
+
 end
